ac76471780f7116170d7e97095bfd5982f835d45
mianusama110 <mianusama011@gmail.com>
2022-03-19 15:56:38 +0500

    added onchange

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -16,7 +16,7 @@ class session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(defaults=fields.date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor",
@@ -25,6 +25,7 @@ class session(models.Model):
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    active = fields.Boolean(default=True)
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -33,6 +34,24 @@ class session(models.Model):
                 r.taken_seats = 0.0
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+
+    @api.onchange('seats','attendee_ids')
+    def _varify_valid_seats(self):
+        if self.seats<0:
+            return {
+                'warning':{
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                }
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
 
 
 
